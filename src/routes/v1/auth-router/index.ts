fa990d1444a7df7eb0e90134361e2fd0176e797a
Louis <79896040+LouisDev314@users.noreply.github.com>
2026-02-04 15:48:47 -0700

    test: github actions ci

--- src/routes/v1/auth-router/index.ts
+++ src/routes/v1/auth-router/index.ts
@@ -8,4 +8,10 @@ const authLimiter = rateLimiter(15,
   'Too many authentication attempts. Please try again later.',
 );
 
+// authRouter.post('/verify-email', emailBodyValidation, async (req, res) => {
+//   const { email, isResetPassword } = req.body as Pick<IUser, 'email'> & { isResetPassword: boolean };
+//   await verifyEmail(email, isResetPassword);
+//   return res.send_ok('Email verified');
+// });
+
 export default authRouter;
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import authRouter from './auth-router';
 
 const v1Router: Router = Router();
+
+v1Router.use('/auth', authRouter);
 
 export default v1Router;
