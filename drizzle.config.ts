b51d359155ee84f6ed1d80a159c17e00ce68ef26
Louis <79896040+LouisDev314@users.noreply.github.com>
2026-03-11 12:46:28 -0600

    refactor

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'drizzle-kit';
+import getEnvConfig from './src/config/env';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './supabase/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: getEnvConfig().databaseUrl,
+  },
+});
