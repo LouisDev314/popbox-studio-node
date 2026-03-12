fe10302fdae6ae946fc66415c4de272999663990
Louis <79896040+LouisDev314@users.noreply.github.com>
2026-03-12 13:17:44 -0600

    fix(qa): phase 8

--- src/routes/v1/health-router/index.ts
+++ src/routes/v1/health-router/index.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
+import { pg } from '../../../db';
 
 const healthRouter: Router = Router();
 
-healthRouter.get('/', (_req, res) => {
-  res.send_ok('Healthy', {
-    uptime: process.uptime(),
-    timestamp: Date.now(),
-  });
+healthRouter.get('/', async (_req, res) => {
+  try {
+    await pg`select 1`;
+    return res.send_ok('Healthy', {
+      uptime: process.uptime(),
+      timestamp: Date.now(),
+    });
+  } catch {
+    return false;
+  }
 });
 
 export default healthRouter;
